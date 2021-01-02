d0631738dae852a99d7cdd8dc33c00e532ddc998
Tarcio Almeida Lima <tarcio.lima@meta.com.br>
2021-01-01 23:29:53 -0300

    Configurando banco h2

--- src/main/java/com/orangetalents/pessoaapi/entity/Pessoa.java
+++ src/main/java/com/orangetalents/pessoaapi/entity/Pessoa.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "pessoa")
+@Table(name = "tb_pessoa")
 public class Pessoa implements Serializable {
 	private static final long serialVersionUID = 1L;
 
