9b760e01c277f139732b2fd3140850ad05ce4687
Tarcio Almeida Lima <tarcio.lima@meta.com.br>
2021-01-02 14:04:33 -0300

    Implementando a validação dos campos

--- src/main/java/com/orangetalents/pessoaapi/controller/PessoaController.java
+++ src/main/java/com/orangetalents/pessoaapi/controller/PessoaController.java
@@ -1,18 +1,16 @@
 package com.orangetalents.pessoaapi.controller;
 
-import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.orangetalents.pessoaapi.dto.PessoaDTO;
 import com.orangetalents.pessoaapi.entity.Pessoa;
@@ -26,10 +24,9 @@ public class PessoaController {
 	private PessoaService pessoaService;
 	
 	@PostMapping
-	public  ResponseEntity<PessoaDTO> save(@RequestBody PessoaDTO dto) {
+	public  ResponseEntity<PessoaDTO> save(@RequestBody @Valid PessoaDTO dto) {
 		dto = pessoaService.save(dto);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
-		return ResponseEntity.created(uri).body(dto);
+		return ResponseEntity.ok().body(dto);
 	}
 	
 	@GetMapping
--- src/main/java/com/orangetalents/pessoaapi/entity/Pessoa.java
+++ src/main/java/com/orangetalents/pessoaapi/entity/Pessoa.java
@@ -1,14 +1,18 @@
 package com.orangetalents.pessoaapi.entity;
 
 import java.io.Serializable;
-import java.time.Instant;
 import java.time.LocalDate;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.br.CPF;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
@@ -20,10 +24,20 @@ public class Pessoa implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(nullable = false, length = 150)
+	@NotEmpty(message = "{campo.nome.obrigatorio}")
 	private String nome;
+	
+	@Column(nullable = false, length = 150)
+	@NotEmpty(message = "{campo.email.obrigatorio}")
 	private String email;
+	
+	@NotNull(message = "{campo.cpf.obrigatorio}")
+	@CPF(message = "{campo.cpf.invalido}")
 	private String cpf;
 	
+	@NotNull(message = "{campo.dataNascimento.obrigatorio}")
 	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate  dataNascimento;
 
